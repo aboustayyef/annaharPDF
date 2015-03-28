ca44eac8e926ac3108a1fe9e3d1c173927948484
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-03-28 12:48:58 +0000

    new, simpler method that relies on bash and curl

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
 
+$content = shell_exec('curl http://newspaper.annahar.com/pdf | grep http://static.annahar.com/storage/pdfs');
+$test = preg_match("#(http://.+\.pdf)#", $content, $result);
 
-// Annahar's PDF No 300 was on April 4th 2014
-$pdfFileNumber = 301 + getDaysSince('04-04-2014');
-$found = 'no';
-
-// keep going back in time until issue is found
-for ($tries=0; $tries < 10 ; $tries++) {
-	$filename = 'http://static.annahar.com/storage/pdfs/2014/'.($pdfFileNumber-$tries).'_file1.pdf';
-	$handle = @fopen($filename, 'r');
-	if ($handle) {
-		$found = 'yes';
-		$annaharPdfFile = $filename;
-		header("location: $annaharPdfFile");
-		break;
-	}
-}
-echo "sorry, couldn't find any pdf from the last 10 days";
-
-
-function getDaysSince($date){
-	$today = new dateTime("now");
-	$wantedDate = new DateTime($date);
-	$difference = $today->diff($wantedDate);
-	return $difference->days;
-}
+$page = $result[0];
+header("location: $page");
 
 ?>
