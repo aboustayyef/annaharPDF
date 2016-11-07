3d437399d8b80c22cdd98a79f716933299636050
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-11-07 19:42:32 +0000

    changed name of file from index.php to send.php

--- mailer.php
+++ mailer.php
@@ -1,9 +1,11 @@
 <?php 
 
 function mailPaper($location){
-	
+
 	$to = 'mustapha.hamoui@gmail.com';
+
 	$subject = 'Today\'s Annahar Newspaper';
+	
 	$headers = "From: mustapha@hamoui.com" . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
@@ -12,6 +14,9 @@ function mailPaper($location){
 	$message .= '<h1>Hey Mus!</h1>';
 	$message .= '<p><a href="' . $location . '">Here</a> is today\'s newspaper. Enjoy! </p>';
 	$message .= '</body></html>';
+
+	mail($to, $subject, $message, $headers);
+
 }
 
 
